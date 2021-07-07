a2e18ed720549df8eddb49f6b0e5bd56108d9e3c
TOM.L <goodjun@163.com>
2021-07-07 06:20:34 -0700

    create meeting

--- src/screens/meeting/index.tsx
+++ src/screens/meeting/index.tsx
@@ -1,12 +1,109 @@
 import React from 'react';
-import { RouteComponentProps } from 'react-router-dom';
-import { Button } from '@material-ui/core';
+import { Box } from '@material-ui/core';
+import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
 import { PageScreen } from '../../components/page-screen/index';
+import { StatusBar } from './components/status-bar';
+import * as styles from './styles';
+import { FooterToolbar } from './components/footer-toolbar';
+import { WebRTC } from './components/web-rtc';
+import { useStores } from '../../contexts/root-context';
+import { MeetingProvider } from './context';
+import Env from '../../config/env';
 
-export const Meeting = ({ history }: RouteComponentProps) => {
+interface IUserSession {
+  id: string;
+  userName: string;
+  isSelf: boolean;
+}
+
+interface IUser {
+  id: string;
+  userName: string;
+}
+
+// TODO: temp meeting info
+const username = 'tom';
+const meetingNumber = '35507';
+
+export const Meeting: React.FC = () => {
+  const { userStore } = useStores();
+
+  const serverRef = React.useRef<HubConnection>();
+
+  const [userSessions, setUserSessions] = React.useState<IUserSession[]>([]);
+
+  const createUserSession = (user: IUser, isSelf: boolean) => {
+    const userSession: IUserSession = {
+      id: user.id,
+      userName: user.userName,
+      isSelf,
+    };
+
+    setUserSessions((oldUserSessions: IUserSession[]) => [
+      ...oldUserSessions,
+      userSession,
+    ]);
+  };
+
+  const removeUserSession = (id: string) => {
+    setUserSessions((oldUserSessions: IUserSession[]) =>
+      oldUserSessions.filter(
+        (userSession: IUserSession) => userSession.id !== id
+      )
+    );
+  };
+
+  React.useEffect(() => {
+    const wsUrl = `${Env.apiUrl}meetingHub?username=${username}&meetingNumber=${meetingNumber}`;
+
+    serverRef.current = new HubConnectionBuilder()
+      .withUrl(wsUrl, { accessTokenFactory: () => userStore.idToken })
+      .build();
+
+    serverRef.current.on('SetLocalUser', (localUser: IUser) => {
+      createUserSession(localUser, true);
+    });
+
+    serverRef.current.on('SetOtherUsers', (otherUsers: IUser[]) => {
+      otherUsers.forEach((user: IUser) => {
+        createUserSession(user, false);
+      });
+    });
+
+    serverRef.current.on('OtherJoined', (otherUser: IUser) => {
+      createUserSession(otherUser, false);
+    });
+
+    serverRef.current.on('OtherLeft', (connectionId: string) => {
+      removeUserSession(connectionId);
+    });
+
+    serverRef.current.start();
+
+    return () => {
+      serverRef.current?.stop();
+    };
+  }, []);
+
   return (
-    <PageScreen>
-      <Button onClick={() => history.goBack()}>Back</Button>
+    <PageScreen style={styles.root}>
+      <MeetingProvider>
+        <StatusBar />
+        <Box style={styles.webRTCContainer}>
+          {userSessions.map((userSession, key) => {
+            return (
+              <WebRTC
+                key={key.toString()}
+                serverRef={serverRef}
+                id={userSession.id}
+                userName={userSession.userName}
+                isSelf={userSession.isSelf}
+              />
+            );
+          })}
+        </Box>
+        <FooterToolbar />
+      </MeetingProvider>
     </PageScreen>
   );
 };
---
+++ src/screens/meeting/styles.ts
@@ -0,0 +1,14 @@
+import { CSSProperties } from 'react';
+
+export const root: CSSProperties = {
+  padding: 0,
+  backgroundColor: '#fafafa',
+  height: '100%',
+};
+
+export const webRTCContainer: CSSProperties = {
+  height: '80%',
+  overflow: 'auto',
+  padding: '10px',
+  textAlign: 'center',
+};
