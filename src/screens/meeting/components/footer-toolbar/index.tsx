ccdbf2c6c63ebdf59e3e608d13a484009112400a
TOM.L <goodjun@163.com>
2021-07-07 06:19:25 -0700

    update FooterToolbar

--- src/screens/meeting/components/footer-toolbar/index.tsx
+++ src/screens/meeting/components/footer-toolbar/index.tsx
@@ -1,16 +1,19 @@
-import { Box, Button, Grid, withStyles } from '@material-ui/core';
+import { Box, Button, Grid, withStyles, ButtonProps } from '@material-ui/core';
 import React from 'react';
 import MicIcon from '@material-ui/icons/Mic';
 import VideocamIcon from '@material-ui/icons/Videocam';
+import MicOffIcon from '@material-ui/icons/MicOff';
+import VideocamOffIcon from '@material-ui/icons/VideocamOff';
 import * as styles from './styles';
+import { MeetingContext } from '../../context';
 
-interface IToolbarButton {
+interface IToolbarButton extends ButtonProps {
   text: string;
   icon: React.ReactElement;
 }
 
 const ToolbarButton = (props: IToolbarButton) => {
-  const { text, icon } = props;
+  const { text, icon, ...rest } = props;
 
   const CButton = withStyles({
     root: {
@@ -19,7 +22,7 @@ const ToolbarButton = (props: IToolbarButton) => {
   })(Button);
 
   return (
-    <CButton size="small">
+    <CButton size="small" {...rest}>
       <Grid container direction="column" justify="center">
         <Grid item>{icon}</Grid>
         <Grid item>{text}</Grid>
@@ -29,15 +32,25 @@ const ToolbarButton = (props: IToolbarButton) => {
 };
 
 export const FooterToolbar = () => {
+  const { video, setVideo, voice, setVoice } = React.useContext(MeetingContext);
+
   return (
     <Box style={styles.footerToolbarContainer}>
       <Grid container direction="row">
         <Grid item container xs={6} justify="flex-start" spacing={1}>
           <Grid item>
-            <ToolbarButton text="声音" icon={<MicIcon />} />
+            <ToolbarButton
+              onClick={() => setVoice(!voice)}
+              text={voice ? '静音' : '解除静音'}
+              icon={voice ? <MicIcon /> : <MicOffIcon />}
+            />
           </Grid>
           <Grid item>
-            <ToolbarButton text="视频" icon={<VideocamIcon />} />
+            <ToolbarButton
+              onClick={() => setVideo(!video)}
+              text={video ? '关闭视频' : '开启视频'}
+              icon={video ? <VideocamIcon /> : <VideocamOffIcon />}
+            />
           </Grid>
         </Grid>
         <Grid item container xs={6} justify="flex-end" spacing={3}>
