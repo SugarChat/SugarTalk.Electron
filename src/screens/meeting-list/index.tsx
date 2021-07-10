cb2846dcc2e407a41a4beeb3517d7e731878ff98
goodjun <goodjun@163.com>
2021-07-10 10:23:06 +0800

    remove screen scss style

--- src/screens/meeting-list/index.tsx
+++ src/screens/meeting-list/index.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import { Settings } from '@material-ui/icons';
-import { Button } from '@material-ui/core';
+import { Button, Divider } from '@material-ui/core';
 import Api from '../../services/api';
-import styles from './index.scss';
 import { Header } from '../../components/header';
+import { PageScreen } from '../../components/page-screen';
+import { useStores } from '../../contexts/root-context';
 
 export const MeetingList = () => {
   const history = useHistory();
+  const { dispatch } = useStores();
 
   const joinMeeting = () => {
     history.push('/JoinMeeting');
+  };
+
+  const logout = () => {
+    dispatch({
+      type: 'UpdateIdToken',
+      payload: '',
+    });
   };
 
   const onScheduleMeetingClicked = async () => {
@@ -19,16 +27,27 @@ export const MeetingList = () => {
   };
 
   return (
-    <div className={styles.root}>
+    <PageScreen>
       <Header title="SugarTalk" />
-      <div className={styles.settingWrapper}>
-        <Settings style={{ color: '#333', width: '25px', height: '25px' }} />
-      </div>
-      <div className={styles.content}>
-        {/* <Button onClick={() => history.goBack()}>Back</Button> */}
-        <Button onClick={joinMeeting}>加入会议</Button>
-        <Button onClick={onScheduleMeetingClicked}>快速会议</Button>
-      </div>
-    </div>
+      <Button
+        color="primary"
+        variant="contained"
+        onClick={joinMeeting}
+        fullWidth
+        disableElevation
+      >
+        加入会议
+      </Button>
+      <Divider />
+      <Button
+        color="primary"
+        variant="contained"
+        onClick={logout}
+        fullWidth
+        disableElevation
+      >
+        注销
+      </Button>
+    </PageScreen>
   );
 };
--- src/screens/schedule-meeting/index.tsx
+++ src/screens/schedule-meeting/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import styles from './index.scss';
 
 export const ScheduleMeeting = () => {
-  return <div className={styles.root}>Schedule a meeting</div>;
+  return <div>Schedule a meeting</div>;
 };
